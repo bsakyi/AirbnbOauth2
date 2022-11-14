61505024bb5cb15bf087032cb97389394c771d75
swiches <donasare@live.com>
2022-11-14 13:52:45 +0000

    auth2 config

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem "jbuilder"
 
 #Authentication
 gem "devise"
+gem "omniauth-google-oauth2"
+ 
+gem 'omniauth-rails_csrf_protection', '~> 1.0'
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'static_pages/home'
+    devise_for :users
+    #,  controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+    root "static_pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
